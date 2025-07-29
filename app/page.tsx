f6414bedee2f21bb8fe0f5463ec0f9ed5fffcb66
samantasubedi <samantasubedi01@gmail.com>
2025-07-29 20:51:17 +0545

    woriking on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,81 @@
-import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <div className="flex flex-row justify-between p-5">
+        <span className="text-4xl text-blue-700 font-bold">ExpenseLog</span>
+        <div className="flex gap-4">
+          <button className="font-semibold text-white bg-blue-800 text-bold rounded-2xl p-2 cursor-pointer hover:bg-blue-600 transition-colors ease-in-out duration-300">
+            Sign up
+          </button>
+          <button className="font-semibold text-white bg-green-600 text-bold rounded-2xl p-2 cursor-pointer hover:bg-green-500 transition-colors ease-in-out duration-300">
+            Sign In
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="mt-[5%] flex flex-col gap-10">
+        <div className="flex justify-center">
+          <p className="font-bold  text-6xl text-center w-[60%] ">
+            Take Control of Your Money, Track Your Expenses & Live Freely.
+          </p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className=" text-xl ">
+            ExpenseLog empowers you to effortlessly track, categorize, and
+            understand your
+          </p>
+          <p className="text-xl ">
+            spending habits. Make smarter financial decisions, starting today.
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center mt-[5%]">
+        <div className="flex gap-5">
+          <button className="font-semibold text-white bg-green-600 text-bold rounded-2xl p-2 hover:bg-green-500 transition-colors ease-in-out duration-300 cursor-pointer ">
+            Get Started - For Free
+          </button>
+          <button className="font-semibold text-black  bg-gray-300 text-bold rounded-2xl p-2 hover:border-black border-2 border-transparent transition-all cursor-pointer ease-in-out duration-300">
+            Learn More{" "}
+          </button>
+        </div>
+      </div>
+
+      <div className="h-screen w-screen flex justify-center mt-[3%]">
+        <div className="bg-blue-100 w-[50%] h-[50%] text-center rounded-2xl p-2">
+          Preview vedio
+        </div>
+      </div>
+      <div className="flex justify-evenly mt-0">
+        <div className="flex flex-col gap-3">
+          <span className="bg-purple-400 size-10 text-center rounded-full p-1 text-2xl font-bold">
+            1
+          </span>
+          <span className="text-xl font-semibold"> Create Your Account</span>
+          <span className="text-xl">
+            Sign up in seconds with your Google account
+          </span>
+        </div>
+
+        <div className="flex flex-col gap-3">
+          <span className="bg-pink-400 size-10 text-center rounded-full p-1 text-2xl font-bold">
+            2
+          </span>
+          <span className="text-xl font-semibold"> Create Your Account</span>
+          <span className="text-xl">
+            Sign up in seconds with your Google account
+          </span>
+        </div>
+
+        <div className="flex flex-col gap-3">
+          <span className="bg-red-400 size-10 text-center rounded-full p-1 text-2xl font-bold">
+            3
+          </span>
+          <span className="text-xl font-semibold"> Create Your Account</span>
+          <span className="text-xl">
+            Sign up in seconds with your Google account
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
