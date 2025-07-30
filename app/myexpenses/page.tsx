0a0256d140db974d541b02dee278b935c0db3072
samantasubedi <samantasubedi01@gmail.com>
2025-07-30 23:41:01 +0545

    oauth creditentials added

--- app/myexpenses/page.tsx
+++ app/myexpenses/page.tsx
@@ -1,6 +1,15 @@
+"use client";
 import React from "react";
+import { useSession } from "next-auth/react";
 
 const Expensepage = () => {
-  return <div className="text-8xl">my expense page</div>;
+  const data = useSession();
+  console.log(data);
+  return (
+    <div className="">
+      <div>My Expenses</div>
+      <div>hello </div>
+    </div>
+  );
 };
 export default Expensepage;
