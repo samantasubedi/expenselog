75097f23d6967665e504e0774a3d12a2643dff63
samantasubedi <samantasubedi01@gmail.com>
2025-07-31 18:39:47 +0545

    converting to client component for interactivity

--- app/myexpenses/page.tsx
+++ app/myexpenses/page.tsx
@@ -1,29 +1,30 @@
-import React from "react";
+//  THE COMMENTED PART IS HOW WE ACCESS SESSION IN SERVER COMPONENT, IT WAS NOT RELAVENT FOR ME TO MAKE IT SEVER COMPONENT SO I CONVERTED IT TO CLIENT COMPONENT
+// import React from "react";
+// import { getServerSession } from "next-auth";
+// import { authoptions } from "../api/auth/[...nextauth]/route";
+// import { GetServerSidePropsContext } from "next";
+// import { signOut } from "next-auth/react";
 
-import { getServerSession } from "next-auth";
-import { authoptions } from "../api/auth/[...nextauth]/route";
-import { GetServerSidePropsContext } from "next";
+// export async function getserversideprops(context: GetServerSidePropsContext) {
+//   // it a special Next.js server-side rendering function that runs on each request to the page.
+//   //The context parameter contains info about the incoming HTTP request, like:
+//   //  context.req — the Node.js request object
+//   // context.res — the Node.js response object
+//   // context.params — route parameters
+//   const session = await getServerSession(context.req, context.res, authoptions); //in frontend we use useSession() with no parameters because
+//   // the browser already knows the current request context (cookies, headers) and useSession handles all that for you.
+//   //but On the backend, there is no automatic knowledge of the current HTTP request — you have to explicitly provide the request and response objects
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: "/signin?error=unauthorized",
+//         permanent: false,
+//       },
+//     };
+//   }
+// }
 import { signOut } from "next-auth/react";
-
-export async function getserversideprops(context: GetServerSidePropsContext) {
-  // it a special Next.js server-side rendering function that runs on each request to the page.
-  //The context parameter contains info about the incoming HTTP request, like:
-  //  context.req — the Node.js request object
-  // context.res — the Node.js response object
-  // context.params — route parameters
-  const session = await getServerSession(context.req, context.res, authoptions); //in frontend we use useSession() with no parameters because
-  // the browser already knows the current request context (cookies, headers) and useSession handles all that for you.
-  //but On the backend, there is no automatic knowledge of the current HTTP request — you have to explicitly provide the request and response objects
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/signin?error=unauthorized",
-        permanent: false,
-      },
-    };
-  }
-}
-
+import React from "react";
 const Expensepage = () => {
   return (
     <div className="">
