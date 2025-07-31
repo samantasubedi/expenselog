3b2e0e1d8bf1a064a06d51988c13edcee05a3606
samantasubedi <samantasubedi01@gmail.com>
2025-07-31 18:36:37 +0545

    protecting routes and making it accessible to signnedin users only

--- app/myexpenses/page.tsx
+++ app/myexpenses/page.tsx
@@ -1,14 +1,40 @@
-"use client";
 import React from "react";
-import { useSession } from "next-auth/react";
+
+import { getServerSession } from "next-auth";
+import { authoptions } from "../api/auth/[...nextauth]/route";
+import { GetServerSidePropsContext } from "next";
+import { signOut } from "next-auth/react";
+
+export async function getserversideprops(context: GetServerSidePropsContext) {
+  // it a special Next.js server-side rendering function that runs on each request to the page.
+  //The context parameter contains info about the incoming HTTP request, like:
+  //  context.req — the Node.js request object
+  // context.res — the Node.js response object
+  // context.params — route parameters
+  const session = await getServerSession(context.req, context.res, authoptions); //in frontend we use useSession() with no parameters because
+  // the browser already knows the current request context (cookies, headers) and useSession handles all that for you.
+  //but On the backend, there is no automatic knowledge of the current HTTP request — you have to explicitly provide the request and response objects
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin?error=unauthorized",
+        permanent: false,
+      },
+    };
+  }
+}
 
 const Expensepage = () => {
-  const data = useSession();
-  console.log(data);
   return (
     <div className="">
       <div>My Expenses</div>
       <div>hello </div>
+      <button
+        className="bg-red-500 text-white font-bold p-2 rounded-2xl cursor-pointer"
+        onClick={() => signOut({ callbackUrl: "/" })}
+      >
+        Sign out
+      </button>
     </div>
   );
 };
