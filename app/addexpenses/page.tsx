d5507eafa026d5bc78434ab67bfce01f28d7b3bc
samantasubedi <samantasubedi01@gmail.com>
2025-08-15 22:03:15 +0545

    conditional form rendering

--- app/addexpenses/page.tsx
+++ app/addexpenses/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Icon } from "@iconify/react";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
@@ -13,6 +13,24 @@ const Addexpensepage = () => {
   if (session.status === "unauthenticated") {
     return redirect("/");
   }
+  const [step, setstep] = useState(1);
+  const [formdata, setformdata] = useState({
+    expensetitle: "",
+    amount: 0,
+    date: "",
+    category: "",
+    description: "",
+  });
+  //this is the function that will be passed as prop to each form
+  const handlenext = (data: Partial<typeof formdata>) => {
+    //here we will obtain a data prameter whose type will be formdata and
+    //  the properties form the formdata will be optional as in first form we will only obtain title and date and so on for other forms
+    setformdata((prev) => ({ ...formdata, ...data })); //this spreads all previous data and adds new data to it.
+    setstep((currentstep) => currentstep + 1); //increasing the step as user presses next
+  };
+  const handleback = () => {
+    setstep((currentstep) => currentstep - 1);
+  };
   return (
     <div className="mb-[2%]">
       <div className="text-3xl text-red-950 font-semibold text-center m-[3%]">
@@ -53,9 +71,11 @@ const Addexpensepage = () => {
       </div>
       <div className="flex justify-center items-center mt-[2%]">
         {/* <Firstform></Firstform> */}
-
         {/* <Secondform></Secondform> */}
-        <Thirdform></Thirdform>
+        {/* <Thirdform></Thirdform> */}
+        {step == 1 && <Firstform></Firstform>}
+        {step == 2 && <Secondform></Secondform>}
+        {step == 3 && <Thirdform></Thirdform>}
       </div>
     </div>
   );
