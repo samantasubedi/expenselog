a146b0d73b5501a4a35ea3dd57bc9ba150b3d6d1
samantasubedi <samantasubedi01@gmail.com>
2025-08-15 23:19:57 +0545

    validating form

--- components/firstform.tsx
+++ components/firstform.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Icon } from "@iconify/react";
 import { firstformschema } from "@/app/schema/validationschema";
 import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 type FormDataType = {
   expensetitle: string;
   amount: number;
@@ -14,7 +16,19 @@ type FirstFormProps = {
 };
 const Firstform = (onNext: FirstFormProps) => {
   type firstformdata = z.infer<typeof firstformschema>; //it extracts the typescript type form zod schema called firstformschema
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<firstformdata>({ resolver: zodResolver(firstformschema) }); // useForm() returns a bunch of methods and state for managing your form.
+  //and we destructure the returned object to obtain various properties like register, handlesubmit etc.
+  //--------register is hte Function you use on each input to “register” it with React Hook Form.
+  //--------handleSubmit is the Function you wrap your submit handler with.
+  //It automatically: Prevents default form submission,Validates the data using Zod,Calls your submit handler with validated data if everything is correct.
+  //---------formState is an object that contains the current state of the form, including validation errors.
+  //<firstformdata> is a TypeScript generic — it tells React Hook Form the exact shape of your form data, so you get full type safety.
+  //The resolver option tells React Hook Form how to validate your form.
+  //connects your Zod schema to React Hook Form.
   return (
     <div className="bg-gradient-to-r from-neutral-200 to-gray-200 p-5 border-1 shadow-md shadow-gray-500 rounded-2xl w-[40%] hover:shadow-xl transition-all duration-300 ease-in-out hover:shadow-neutral-600">
       <div className="flex flex-col gap-5">
